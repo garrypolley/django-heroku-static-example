1d8170209b3bb15c5102c7a3d817f9a82cbeebca
Garry Polley <garrympolley@gmail.com>
2012-10-18 06:50:58 -0500

    adding in static files server.

--- dhs/urls.py
+++ dhs/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import patterns, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic.base import TemplateView
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
 )
+
+urlpatterns += staticfiles_urlpatterns()
